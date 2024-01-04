2582880382f39009ca0597f0240558d99db38c48
aswinthomas <aswinthomas@gmail.com>
2024-01-04 14:00:40 +0800

    refactor to composable

--- app/src/main/java/com/aswindev/createlist/MainActivity.kt
+++ app/src/main/java/com/aswindev/createlist/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -38,39 +39,55 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize()
                         .padding(16.dp)
                 ) {
-                    Row (
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        OutlinedTextField(
-                            value = name, 
-                            onValueChange = { text ->
-                                name = text
-                            },
-                            modifier = Modifier.weight(1f)
-                        )
-                        Spacer(modifier = Modifier.width(16.dp))
-                        Button(onClick = {
-                            if (name.isNotBlank()) {
-                                names = names + name
-                                Log.i("MyInfo", names.toString())
-                                name = ""
-                            }
-                        }) {
-                            Text(text = "Add")
-                        }
-                    }
-                    LazyColumn {
-                        items(names) {currName ->
-                            Text(
-                                text = currName,
-                                modifier = Modifier.fillMaxWidth()
-                                    .padding(16.dp)
-                            )
-                            Divider()
-                        }
-                    }
-
+                    names = setTopRow(name, names)
+                    SetNameList(names)
                 }
+            }
+        }
+    }
+
+    @Composable
+    private fun setTopRow(
+        name: String,
+        names: List<String>
+    ): List<String> {
+        var name1 = name
+        var names1 = names
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                value = name1,
+                onValueChange = { text ->
+                    name1 = text
+                },
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Button(onClick = {
+                if (name1.isNotBlank()) {
+                    names1 = names1 + name1
+                    Log.i("MyInfo", names1.toString())
+                    name1 = ""
+                }
+            }) {
+                Text(text = "Add")
+            }
+        }
+        return names1
+    }
+
+    @Composable
+    private fun SetNameList(names: List<String>) {
+        LazyColumn {
+            items(names) { currName ->
+                Text(
+                    text = currName,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+                Divider()
             }
         }
     }
